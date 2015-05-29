c4d7c3d2426ed0b3691ab7d12cb2856cbcb33ff6
Ryan Strat <ryanstrat@users.noreply.github.com>
2015-05-29 16:34:11 +0200

    Added more configurable options and rename config file
    Begin to add database information

--- send.php
+++ send.php
@@ -1,17 +1,42 @@
 <?php
 
 require "twilio-php-master/Services/Twilio.php";
-require_once "credentials.php";
+require_once "config.php";
 
 
-$client = new Services_Twilio($AccountSid, $AuthToken);
+
+
+$mysqli = new mysqli($HOST, $USER, $PASSWORD, $DATABASE);
+$client = new Services_Twilio($ACCOUNT_SID, $AUTH_TOKEN);
+
+$target = "ryan"; // temp set static target
+if($_REQUEST['To'] == $US_NUMBER) {
+    $outboundNumber = $FRENCH_NUMBER;
+    $targetPhone = "french_phone";
+} else if($_REQUEST['To'] == $FRENCH_NUMBER) {
+    $outboundNumber = $US_NUMBER;
+    $targetPhone = "us_phone";
+} else {
+    error_log("MSG received on unknown phone number. Number: ".$_REQUEST['To']);
+    exit(1); //Kill the program if source can't be reliably confirmed
+}
+
+    
+if($result = $mysqli->query("SELECT ".$targetPhone." FROM users WHERE handle=".$target)) {
+    $obj = $result->fetch_object();
+    $targetPhoneNumber = $obj[$targetPhone];
+} else {
+    echo $mysqli->error;
+    error_log($mysqli->error);
+}
 
 try {
     $message = $client->account->messages->create(array(
-        "From" => "+14703446969",
+        "From" => $outboundNumber,
         "To" => "+16785173393",
         "Body" => $_REQUEST['Body'],
     ));
 } catch (Services_Twilio_RestException $e) {
     echo $e->getMessage();
+    error_log($e->getMessage());
 }
